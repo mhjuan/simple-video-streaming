656c9d7f598df72860665e4083734227c1d7f33f
MingHao Juan <howardjuan0118@gmail.com>
2019-05-14 15:38:40 +0800

    Prototype done

---
+++ README.md
@@ -0,0 +1,6 @@
+# Simple Video Streaming Using Node.js and HTML5
+
+## Install
+- `npm i`
+- `npm start` (use `sudo` since the port is set to 80, or change to a port over 1024)
+- Open browser and go to `localhost`
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const port = 80;
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => res.sendFile('index.html'));
+
+app.get('/video', (req, res) => {
+  const path = 'assets/vasa.mp4';
+  const stat = fs.statSync(path);
+  const fileSize = stat.size;
+  const range = req.headers.range;
+
+  if (range) {
+    const parts = range.replace(/bytes=/, '').split('-');
+    const start = parseInt(parts[0], 10);
+    const end = (parts[1] ? parseInt(parts[1], 10) : fileSize - 1);
+    // console.log(start, end)
+
+    const chunkSize = end - start + 1;
+    const file = fs.createReadStream(path, {start, end});
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunkSize,
+      'Content-Type': 'video/mp4',
+    };
+
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    };
+    res.writeHead(200, head);
+    fs.createReadStream(path).pipe(res);
+  }
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
